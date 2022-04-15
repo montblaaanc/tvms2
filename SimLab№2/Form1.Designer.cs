f036ca27917f75171ccb35631fbe85efdc0a7ab8
montblaaanc <block111@bk.ru>
2022-04-15 22:20:10 +0500

    primipls

--- SimLab№2/Form1.Designer.cs
+++ SimLab№2/Form1.Designer.cs
@@ -378,7 +378,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
             this.ClientSize = new System.Drawing.Size(1137, 664);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
